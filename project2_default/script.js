580eaa4b0d0185b07c60563628cb9fcde7304994
Goeun Park <goeun0179@gmail.com>
2018-02-06 14:50:59 -0500

    Add files via upload

---
+++ project2_default/script.js
@@ -0,0 +1,118 @@
+$(document).ready(function(){
+
+
+	$('.ind').click(function(){
+		console.log('click detected');
+		$('#revealtop').toggle();
+	});
+
+
+	$('.large').click(function(){
+		console.log('click detected');
+		$('.box').toggleClass('reveal');
+	});
+
+	$('.column1 .medium').click(function(){
+		console.log('click detected');
+		$('.column1 .box2').toggleClass('reveal');
+	});
+
+	$('.column1 .small').click(function(){
+		console.log('click detected');
+		$('.column1 .box3').toggleClass('reveal');
+	});
+
+	$('.column1 .smaller').click(function(){
+		console.log('click detected');
+		$('.column1 .box4').toggleClass('reveal');
+	});
+
+	$('.column1 .smallest').click(function(){
+		console.log('click detected');
+		$('.column1 .box5').toggleClass('reveal');
+	});
+
+	$('.column1 .smallest2').click(function(){
+		console.log('click detected');
+		$('.column1 .box6').toggleClass('reveal');
+	});
+
+	// column2
+
+	$('.large').click(function(){
+		console.log('click detected');
+		$('.column2 .box').toggleClass('reveal');
+	});
+
+	$('.column2 .medium').click(function(){
+		console.log('click detected');
+		$('.column2 .box2').toggleClass('reveal');
+	});
+
+	$('.column2 .small').click(function(){
+		console.log('click detected');
+		$('.column2 .box3').toggleClass('reveal');
+	});
+
+	$('.column2 .smaller').click(function(){
+		console.log('click detected');
+		$('.column2 .box4').toggleClass('reveal');
+	});
+
+	$('.column2 .smallest').click(function(){
+		console.log('click detected');
+		$('.column2 .box5').toggleClass('reveal');
+	});
+
+	$('.column2 .smallest2').click(function(){
+		console.log('click detected');
+		$('.column2 .box6').toggleClass('reveal');
+	});
+
+	// column3
+
+	$('.large').click(function(){
+		console.log('click detected');
+		$('.column3 .box').toggleClass('reveal');
+	});
+
+	$('.column3 .medium').click(function(){
+		console.log('click detected');
+		$('.column3 .box2').toggleClass('reveal');
+	});
+
+	$('.column3 .small').click(function(){
+		console.log('click detected');
+		$('.column3 .box3').toggleClass('reveal');
+	});
+
+	$('.column3 .smaller').click(function(){
+		console.log('click detected');
+		$('.column3 .box4').toggleClass('reveal');
+	});
+
+	$('.column3 .smallest').click(function(){
+		console.log('click detected');
+		$('.column3 .box5').toggleClass('reveal');
+	});
+
+	$('.column3 .smallest2').click(function(){
+		console.log('click detected');
+		$('.column3 .box6').toggleClass('reveal');
+	});
+
+
+
+
+
+
+
+
+
+
+	// $(".element").click(function(){
+	// 	console.log("clicking element");
+	// 	$(this).toggleClass('play');
+	// });
+
+});
